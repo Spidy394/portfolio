a054e803259c1bfb669870b679c9836f47e42709
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-05-24 13:41:56 +0530

    add project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -16,17 +16,15 @@ const Projects = () => {
             {/* project-1 */}
             <div className="p-6 rounded border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2">
-                {/* project title */} Lorem ipsum dolor sit amet.
+                {/* project title */} AI Voice Companion.
               </h3>
               <p className="text-gray-400 mb-4">
-                {/* description */} Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Harum nam, impedit cum eius illum sunt vel
-                adipisci eum explicabo maiores!
+                {/* description */} An AI-powered virtual companion designed to engage users in natural, voice-based conversations. Listens to user speech, transcribes it to text, and responds with contextually appropriate replies in a warm, expressive voice. 
               </p>
 
               {/* stack */}
               <div className="flex flex-wrap gap-2 mb-4">
-                {["js", "py"].map((tech, key) => (
+                {["Python", "Gemini-API", "uv"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -37,7 +35,7 @@ const Projects = () => {
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://github.com/Spidy394/AI-GF.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
